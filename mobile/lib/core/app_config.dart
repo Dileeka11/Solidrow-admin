e0a27a181307498a0b6ee26ed2c46534f680ee3d
Dileeka Supun <dileekasupun11@gmail.com>
2026-07-08 13:55:21 +0530

    feat: add AppConfig class for centralizing API base URL and network timeouts

--- mobile/lib/core/app_config.dart
+++ mobile/lib/core/app_config.dart
@@ -5,7 +5,7 @@ class AppConfig {
   /// For local dev on a physical Android device, use your machine's LAN IP:
   ///   e.g. http://192.168.1.100:8000
   /// For an emulator connecting to host: http://10.0.2.2:8000
-  static const String baseUrl = 'http://10.60.43.194:8000';
+  static const String baseUrl = 'https://registration.solidrow.lk';
 
   static const Duration connectTimeout = Duration(seconds: 15);
   static const Duration receiveTimeout = Duration(seconds: 30);
